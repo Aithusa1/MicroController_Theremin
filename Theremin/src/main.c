54a27e105ac5856e3a7a367c3ce405c1c6def6ef
Aithusa <amyvoosten@hotmail.com>
2025-10-13 14:05:01 +0200

    Basis Code Buzzer
    Basis code vanuit de les toegevoegd om de buzzer op pin 3 geluid te laten maken aan de hand van frequentie.

--- Theremin/src/main.c
+++ Theremin/src/main.c
@@ -1,12 +1,39 @@
-
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdbool.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
 
-int main(void) {
-  while (1) {
+uint8_t freq = 50;
+void setup_timer0(void)
+{
+  TCCR0A |= (1<<WGM01);
+  TCCR0B |= (1<<CS02);
+  OCR0A = 70;
+  TCNT0 = 0;
+  TIMSK0 |= (1<<OCIE0A);
+  sei();
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+  PORTD ^= (1<<PD3);
+}
+
+void afstand_meten()
+{
+
+}
+
+void setup_timer0(void);
+
+int main()
+{
+  int freq = 0;
+  DDRD |= (1<<PD3);
+  setup_timer0();
+
+  while(1)
+  {
+  
   }
-
   return 0;
 }
