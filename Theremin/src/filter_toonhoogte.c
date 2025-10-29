689b40080dcd43cc1782babca334139acadb3fb4
Aithusa <amyvoosten@hotmail.com>
2025-10-29 13:29:37 +0100

    Filter functionaliteit

---
+++ Theremin/src/filter_toonhoogte.c
@@ -0,0 +1,94 @@
+#include <avr/io.h>
+#include <stdlib.h>
+#include "filter_toonhoogte.h"
+
+// Filter buffer volgens technisch ontwerp
+volatile uint8_t filter_size = 5;  // Start met medium filter grootte
+FilterElement filter_buffer[FILTER_MAX_SIZE];
+
+// Compare functie voor qsort (sorteren op value)
+int compare_filter_elements(const void *a, const void *b) {
+    const FilterElement *elemA = (const FilterElement *)a;
+    const FilterElement *elemB = (const FilterElement *)b;
+    
+    if (elemA->value < elemB->value) return -1;
+    if (elemA->value > elemB->value) return 1;
+    return 0;
+}
+
+// Filter initialisatie
+void filter_init(void) {
+    for (uint8_t i = 0; i < FILTER_MAX_SIZE; i++) {
+        filter_buffer[i].value = 800;  // Start op midden frequentie
+        filter_buffer[i].age = i;      // Oudste eerst
+    }
+}
+
+// Waarde toevoegen aan filter volgens technisch ontwerp algoritme
+void filter_add_value(uint16_t value) {
+    // Verhoog leeftijd van alle elementen
+    for (uint8_t i = 0; i < filter_size; i++) {
+        if (filter_buffer[i].age < 255) {
+            filter_buffer[i].age++;
+        }
+    }
+    
+    // Zoek oudste element
+    uint8_t oldest_index = 0;
+    uint8_t oldest_age = filter_buffer[0].age;
+    
+    for (uint8_t i = 1; i < filter_size; i++) {
+        if (filter_buffer[i].age > oldest_age) {
+            oldest_age = filter_buffer[i].age;
+            oldest_index = i;
+        }
+    }
+    
+    // Vervang oudste element met nieuwe waarde
+    filter_buffer[oldest_index].value = value;
+    filter_buffer[oldest_index].age = 0;
+}
+
+// Mediaan bepalen volgens technisch ontwerp
+uint16_t filter_get_median(void) {
+    if (filter_size == 1) {
+        return filter_buffer[0].value;  // Geen filtering als size = 1
+    }
+    
+    // Maak tijdelijke kopie voor sortering
+    FilterElement temp_buffer[FILTER_MAX_SIZE];
+    for (uint8_t i = 0; i < filter_size; i++) {
+        temp_buffer[i] = filter_buffer[i];
+    }
+    
+    // Sorteer op waarde (quicksort volgens technisch ontwerp)
+    qsort(temp_buffer, filter_size, sizeof(FilterElement), compare_filter_elements);
+    
+    // Bepaal mediaan (middelste element)
+    // Volgens technisch ontwerp: (arraylengte / 2) + 1
+    uint8_t median_index = (filter_size / 2);
+    
+    return temp_buffer[median_index].value;
+}
+
+// Filter grootte instellen (alleen oneven getallen volgens functioneel ontwerp)
+void filter_set_size(uint8_t new_size) {
+    if (new_size < FILTER_MIN_SIZE) new_size = FILTER_MIN_SIZE;
+    if (new_size > FILTER_MAX_SIZE) new_size = FILTER_MAX_SIZE;
+    
+    // Zorg voor oneven getallen volgens UC.6
+    if (new_size % 2 == 0) {
+        new_size--;  // Maak oneven
+        if (new_size < FILTER_MIN_SIZE) new_size = FILTER_MIN_SIZE;
+    }
+    
+    filter_size = new_size;
+    
+    // Herinitialiseer filter bij grootte wijziging
+    filter_init();
+}
+
+// Huidige filter grootte opvragen
+uint8_t filter_get_size(void) {
+    return filter_size;
+}
--- Theremin/src/main.c
+++ Theremin/src/main.c
@@ -7,6 +7,7 @@
 #include "weergeven_afstand_hoogte.h"
 #include "bepaal_toonhoogte.h"
 #include "bepaal_volume.h"
+#include "filter_toonhoogte.h"
 
 // Global variables - gedefinieerd in volume.c
 extern volatile uint8_t volume;
@@ -20,15 +21,53 @@ void display_info(uint16_t dist, uint16_t freq) {
     char buffer[16];
     
     if (dist > 0) {
-        snprintf(buffer, sizeof(buffer), "Dist:%3dcm ", dist);
+        snprintf(buffer, sizeof(buffer), "Dist:%3dcm F:%2d", dist, filter_get_size());
     } else {
-        snprintf(buffer, sizeof(buffer), "No signal   ");
+        snprintf(buffer, sizeof(buffer), "No signal   F:%2d", filter_get_size());
     }
     lcd_print(buffer);
     
     lcd_command(0xC0); // Tweede regel
     snprintf(buffer, sizeof(buffer), "F:%4dHz V:%3d", freq, volume);
     lcd_print(buffer);
+}
+
+void setup_filter_buttons(void) {
+    // PD4 en PD5 als input met pull-up (volgens TO)
+    DDRD &= ~((1 << PD4) | (1 << PD5));
+    PORTD |= (1 << PD4) | (1 << PD5);
+    
+    // Pin Change Interrupt enable (TO: PCICR en PCMSK2)
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT20) | (1 << PCINT21);
+}
+
+// Pin Change Interrupt voor filterknoppen (UC.6: Aanpassen filter)
+ISR(PCINT2_vect) {
+    static uint8_t last_state = 0xFF;
+    uint8_t current_state = PIND & ((1 << PD4) | (1 << PD5));
+    
+    // Debounce
+    if(current_state != last_state) {
+        _delay_ms(50);
+        current_state = PIND & ((1 << PD4) | (1 << PD5));
+        
+        if((current_state & (1 << PD4)) == 0) { // PD4 ingedrukt - filter kleiner
+            uint8_t current_size = filter_get_size();
+            if(current_size > FILTER_MIN_SIZE) {
+                filter_set_size(current_size - 1);
+            }
+        }
+        
+        if((current_state & (1 << PD5)) == 0) { // PD5 ingedrukt - filter groter
+            uint8_t current_size = filter_get_size();
+            if(current_size < FILTER_MAX_SIZE) {
+                filter_set_size(current_size + 1);
+            }
+        }
+        
+        last_state = current_state;
+    }
 }
 
 int main(void) {
@@ -40,12 +79,14 @@ int main(void) {
     TWI_Init();
     _delay_ms(1000);
     lcd_init();
+    filter_init();  // UC.2: Filter toonhoogte initialisatie
     
     // Hardware initialisatie VOLGENS TECHNISCH ONTWERP:
-    setup_adc();                    // Volume ADC
+    setup_adc();                    // Volume ADC (UC.3)
     setup_timer2_volume_pwm();      // Timer2: Fast PWM voor volume
     setup_timer0_frequency();       // Timer0: CTC mode voor frequentie
     setup_timer1_sensor();          // Timer1: Sensor meting
+    setup_filter_buttons();         // Filter knoppen (UC.6)
     
     // Start bericht
     lcd_command(0x01);
@@ -55,11 +96,16 @@ int main(void) {
     // Global interrupts inschakelen
     sei();
     
-    // Hoofdloop - SNEL updates
+    // In de hoofdloop:
     while(1) {
         uint16_t dist = read_distance();
         target_freq = dist_to_freq(dist);
-        smooth_freq(&current_freq, target_freq);
+        
+        // FILTER TOEPASSEN volgens use case UC.2
+        filter_add_value(target_freq);
+        uint16_t filtered_freq = filter_get_median();
+        
+        smooth_freq(&current_freq, filtered_freq);
         
         // Update PWM duty cycle op basis van volume
         OCR2B = volume;
